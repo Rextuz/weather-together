0e872d6ebfcfc4fabb894caf49718728d4437de2
Deadlyspark <cherepanovofficial@gmail.com>
2015-11-11 22:21:55 +0300

    Change received units.

--- app/src/main/java/com/rextuz/weathertogether/YahooWeather.java
+++ app/src/main/java/com/rextuz/weathertogether/YahooWeather.java
@@ -38,7 +38,7 @@ public class YahooWeather implements WeatherServiceInterface {
     @Override
     public WeatherEntity getCurrentWeather(final String place) {
         try {
-            String YQL = String.format("select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"%s\")", place);
+            String YQL = String.format("select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"%s\") and u = 'c'", place);
             String endpoint = String.format("https://query.yahooapis.com/v1/public/yql?q=%s&format=json", Uri.encode(YQL));
 
             result = new WeatherTask(endpoint).execute().get();
